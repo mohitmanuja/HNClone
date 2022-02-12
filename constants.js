616acd907e18bef2203892510b5a05cee886e023
Mohit Manuja <manuja.mohit@gmail.com>
2022-02-12 19:35:59 +0530

    Fav/Remove Functionality Added

---
+++ constants.js
@@ -0,0 +1,12 @@
+/* const APIKEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nc3J1d2ZiaW1paHNubG96YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1OTQ0MzgsImV4cCI6MTk2MDE3MDQzOH0.22p_uzytYGW8wZznlLLgB61-JYbBkV-7lcwXm_tMyWc";
+const AUTH =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nc3J1d2ZiaW1paHNubG96YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1OTQ0MzgsImV4cCI6MTk2MDE3MDQzOH0.22p_uzytYGW8wZznlLLgB61-JYbBkV-7lcwXm_tMyWc";
+
+export function getCommonHeader() {
+  return {
+    apikey: APIKEY,
+    Authorization: AUTH,
+  };
+}
+ */
--- script.js
+++ script.js
@@ -1,25 +1,37 @@
+const APIKEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nc3J1d2ZiaW1paHNubG96YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1OTQ0MzgsImV4cCI6MTk2MDE3MDQzOH0.22p_uzytYGW8wZznlLLgB61-JYbBkV-7lcwXm_tMyWc";
+const AUTH =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nc3J1d2ZiaW1paHNubG96YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1OTQ0MzgsImV4cCI6MTk2MDE3MDQzOH0.22p_uzytYGW8wZznlLLgB61-JYbBkV-7lcwXm_tMyWc";
+
 // fetch Initial page
 var page = 1;
 
 var q = {};
+var favData = {};
+var set = new Set();
 
+getFavList();
 
-if (location.href.includes("?")) {
-  location.href
-    .split("?")[1]
-    .split("&")
-    .forEach(function (i) {
-      q[i.split("=")[0]] = i.split("=")[1];
-    });
-  if (q["page"]) {
-    console.log("query page" + page);
-    page = q["page"];
-    fetchData(q["page"]);
+function getInitialData() {
+  console.log(JSON.stringify(favData));
+
+  if (location.href.includes("?")) {
+    location.href
+      .split("?")[1]
+      .split("&")
+      .forEach(function (i) {
+        q[i.split("=")[0]] = i.split("=")[1];
+      });
+    if (q["page"]) {
+      console.log("query page" + page);
+      page = q["page"];
+      fetchData(q["page"]);
+    } else {
+      fetchData(page);
+    }
   } else {
     fetchData(page);
   }
-} else {
-  fetchData(page);
 }
 
 function fetchData(page) {
@@ -53,19 +65,32 @@ function setData(data) {
 function getListItem(element) {
   // li item
   let li = document.createElement("li");
+  let favTag = document.createElement("img");
+  favTag.id = "favButton";
+  favTag.height = 30;
+  favTag.height = 30;
+
+  const isPresent = set.has(element.objectID);
+  if (isPresent) {
+    favTag.innerText = "Remove";
+    favTag.src = "favIconFilled.svg";
+  } else {
+    favTag.innerText = "Fav";
+    favTag.src = "favIcon.svg";
+  }
+
+  // set 10
+  favTag.addEventListener("click", function () {
+    titleClicked(element, favTag); //11 //9
+  });
+
   let titleDiv = document.createElement("div");
   titleDiv.className = "titleDiv";
-  let favTag = document.createElement("button");
-  favTag.innerText = "Fav";
-  favTag.addEventListener("click",titleClicked)
-
-
   let aTitle = document.createElement("a");
   aTitle.innerText = element.title;
   aTitle.href = element.url;
   aTitle.id = "title";
   aTitle.className = "hover-link title";
-  titleDiv.appendChild(favTag);
   titleDiv.appendChild(aTitle);
   li.appendChild(titleDiv);
 
@@ -80,6 +105,7 @@ function getListItem(element) {
 
   linkDiv.appendChild(aTag);
   li.appendChild(linkDiv);
+  li.appendChild(favTag);
 
   // points
   let detailDiv = document.createElement("detail");
@@ -120,18 +146,107 @@ function getListItem(element) {
   return li;
 }
 
-
-function titleClicked(){
-  console.log("ho rha hai ");
+function titleClicked(element, view) {
+  var ourElement = {
+    title: element.title,
+    url: element.url,
+    author: element.author,
+    objectID: element.objectID,
+    points: element.points,
+  };
+  triggerFav(ourElement, view);
 }
 
 function getMoreData(movePage) {
-  if(movePage){
-    page= movePage
+  if (movePage) {
+    page = movePage;
     window.location = "?page=" + movePage;
-
-  }else{
+  } else {
     ++page;
     window.location = "?page=" + page;
+  }
+}
+
+function getCommonHeader() {
+  return {
+    apikey: APIKEY,
+    Authorization: AUTH,
+    "Content-Type": "application/json",
+    Prefer: "return=representation",
+  };
+}
+
+function getFavList() {
+  fetch("https://mgsruwfbimihsnlozaus.supabase.co/rest/v1/favourite?select=*", {
+    headers: getCommonHeader(),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      favData = data;
+      // on
+      data.forEach((element) => {
+        set.add(element.objectID);
+      });
+      getInitialData();
+    })
+    .catch(function () {
+      // handle the error
+    });
+}
+
+function triggerFav(element, view) {
+  const isPresent = set.has(element.objectID);
+  view.classList.toggle("fade");
+  view.innerText = "Loading";
+
+  if (!isPresent) {
+    var audio = new Audio(
+      "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-77317/zapsplat_multimedia_button_click_fast_wooden_organic_002_78836.mp3"
+    );
+    audio.play();
+    fetch("https://mgsruwfbimihsnlozaus.supabase.co/rest/v1/favourite", {
+      method: "POST",
+      headers: getCommonHeader(),
+      body: JSON.stringify(element),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        view.src = "favIconFilled.svg";
+
+        set.add(element.objectID);
+        console.log("data" + JSON.stringify(data));
+      })
+      .catch(function () {
+        // handle the error
+      });
+  } else {
+    var audio = new Audio("aww.mp3");
+    audio.play();
+    // https://mgsruwfbimihsnlozaus.supabase.co/rest/v1/favourite?post_id=eq.test
+    fetch(
+      "https://mgsruwfbimihsnlozaus.supabase.co/rest/v1/favourite?objectID=eq." +
+        element.objectID,
+      {
+        method: "DELETE",
+        headers: getCommonHeader(),
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        set.delete(element.objectID);
+        console.log("removed", set);
+        view.src = "favIcon.svg";
+
+        console.log("data" + JSON.stringify(data));
+      })
+      .catch(function () {
+        // handle the error
+      });
   }
 }
